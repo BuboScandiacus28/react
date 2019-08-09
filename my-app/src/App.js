321962868d8e6f93bd9ee1c8e78773c410d72752
Alex Korolenko <28filosof28@gmail.com>
2019-08-09 16:15:17 +0300

    component testing in Add.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,24 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Body />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<div className="Header">
+			<ul>
+				<li>Home</li>
+				<li>Work</li>
+				<li>Setting</li>
+			</ul>
+		</div>
+	);
+}
+
+function Body() {
+	return (
+		<div className="Body">
+			<p>Simple HTML</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi esse, non vero, quasi pariatur dolor hic aperiam sit vel, ex nesciunt voluptates ipsa porro unde. Laboriosam at vitae cupiditate, ullam.</p>
+		</div>
+	);
 }
 
 export default App;
