32af273f42bcdd4ab523393ca4485f9e087fa90c
Alex Korolenko <28filosof28@gmail.com>
2019-08-09 23:35:10 +0300

    initial app markup added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './header';
-import Body from './body';
 
 function App() {
 	return (
-		<div className="App">
-			<Header />
-			<Body />
+		<div className = "app__wrapper">
+			<header className = "header">
+				<img src="logo.svg" alt=""/>
+			</header>
+			<nav className = "nav">
+				<ul className = "nav__list">
+					<li className = "nav__list-item"><a href="#">Profile</a></li>
+					<li className = "nav__list-item"><a href="#">Messages</a></li>
+					<li className = "nav__list-item"><a href="#">Main content</a></li>
+				</ul>
+				<a href="#">Setting</a>
+			</nav>
+			<section className = "content">
+				<div className = "content__main-img-container">
+					<img className = "content__main-img" src="https://cdn.stocksnap.io/img-thumbs/960w/WMH53WWA3J.jpg" alt=""/>
+				</div>
+				<div className = "content__profile-img-container">
+					<img className = "content__profile-img" src="https://cdn.stocksnap.io/img-thumbs/960w/QO8V4WRO3T.jpg" alt=""/>
+				</div>
+				<div className = "content__user-info">
+
+				</div>
+			</section>
 		</div>
 	);
 }
